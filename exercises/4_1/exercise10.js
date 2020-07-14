bf112327b9efe72828222863a225ab01a667718b
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-07-13 21:23:46 -0300

    Exercicio 11

--- exercises/4_1/exercise10.js
+++ exercises/4_1/exercise10.js
@@ -1,10 +1,30 @@
-let custo = 100;
-let venda = 200;
+var aliquotINSS;
+var aliquotIR;
 
-if (custo || venda <= 0) {
-  lucro = (venda - (custo * 0.8)) * 1000;
+var grossSalary = 3500;
+
+if (grossSalary <= 1556.94) {
+  aliquotINSS = grossSalary * 0.08;
+} else if (grossSalary <= 2594.92) {
+  aliquotINSS = grossSalary * 0.09;
+} else if (grossSalary <= 5189.82) {
+  aliquotINSS = grossSalary * 0.11;
 } else {
-  lucro = "Error";
+  aliquotINSS = 570.88;
 }
 
-console.log(lucro);
+var baseSalary = grossSalary - aliquotINSS;
+
+if (baseSalary <= 1903.98) {
+  aliquotIR = 0;
+} else if (baseSalary <= 2826.65) {
+  aliquotIR = baseSalary * 0.075 - 142.80;
+} else if (baseSalary <= 3751.05) {
+  aliquotIR = baseSalary * 0.15 - 354.80;
+} else if (baseSalary <= 4664.68) {
+  aliquotIR = baseSalary * 0.225 - 636.13;
+} else {
+  aliquotIR = baseSalary * 0.275 - 869.36;
+};
+
+console.log("Salário: " + (baseSalary - aliquotIR));
