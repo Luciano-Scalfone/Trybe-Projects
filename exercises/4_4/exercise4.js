8352a50cbc85e8987a7c90fe07a4fc540ca265a4
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-07-16 18:15:41 -0300

    Exercise 4

---
+++ exercises/4_4/exercise4.js
@@ -0,0 +1,11 @@
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  recorrente: "Sim"
+};
+/*  com for in imprima todas os valores do objeto  */
+
+for (i in info) {
+  console.log(info[i]);
+}
