dc204273ffc88ceda2b554e6629a809b81819593
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-07-16 18:03:12 -0300

    Exercise 2

---
+++ exercises/4_4/exercise2.js
@@ -0,0 +1,12 @@
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+
+/* Inserir a chave de nome "recorrente" e o valor "Sim"
+ imprima o objeto no console*/
+
+ info.recorrente = "Sim";
+
+ console.log(info);
