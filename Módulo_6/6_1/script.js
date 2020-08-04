07f884a50d75ce47743ed597620b8f9909db1e75
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-08-04 08:21:51 -0300

    date limit

--- Módulo_6/6_1/script.js
+++ Módulo_6/6_1/script.js
@@ -1,20 +1,60 @@
-let estados = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO',];
+let estados = {
+  selecione: 'selecione',
+  AC: 'Acre',
+  AL: 'Alagoas',
+  AM: 'Amazonas',
+  AP: 'Amapá',
+  BA: 'Bahia',
+  CE: 'Ceará',
+  DF: 'Distrito Federal',
+  ES: 'Espírito Santo',
+  GO: 'Goiás',
+  MA: 'Maranhão',
+  MG: 'Minas Gerais',
+  MS: 'Mato Grosso do Sul',
+  MT: 'Mato Grosso',
+  PA: 'Pará',
+  PB: 'Paraíba',
+  PE: 'Pernambuco',
+  PI: 'Piauí',
+  PR: 'Paraná',
+  RJ: 'Rio de Janeiro',
+  RN: 'Rio Grande do Norte',
+  RO: 'Rondônia',
+  RR: 'Roraima',
+  RS: 'Rio Grande do Sul',
+  SC: 'Santa Catarina',
+  SE: 'Sergipe',
+  SP: 'São Paulo',
+  TO: 'Tocantins',
+};
 let selectEstado = document.querySelector('#estado');
-let emailInput = document.querySelector('#email');
-
-for (let index = 0; index < estados.length; index += 1) {
+for (const property in estados) {
   let option = document.createElement('option');
-  option.innerHTML = estado[index];
-  option.value = estado[index]
-  estados.appendChild(option);
+  option.value = `${property}`;
+  option.id = `${property}`;
+  option.innerHTML = `${estados[property]}`;
+  selectEstado.appendChild(option);
 }
-
-emailInput.addEventListener('change', checkEmail);
-
-function checkEmail (event) {
-  let email = event.target;
-  let regex = new RegExp(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/)
-  if (email == regex) {
-    
-  }
+function checkMail(event) {
+  const emailText = event.target.value;
+  const regEx = RegExp(/^[a-zA-Z0-9.!#$%&_-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/, 'i');
+  if (!regEx.test(emailText)) {
+    alert("E-mail inválido");
+  };
 }
+let emailInput = document.querySelector('#email');
+emailInput.addEventListener('change', checkMail);
+let today = new Date();
+let actual = document.querySelector('#data-inicio');
+let year = today.getFullYear();
+let month = today.getMonth() + 1;
+let day = today.getDate();
+if (month < 10) {
+  month = '0' + month;
+}
+if (day < 10) {
+  day = '0' + day;
+}
+today = year + '-' + month + '-'  + day;
+actual.setAttribute('max', today);
