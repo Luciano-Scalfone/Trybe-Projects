e44395f00c27270ef317d71cbdc7a60879546a16
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-08-11 13:05:58 -0300

    Exercise 4

---
+++ Módulo_7/7_1/exercise4.js
@@ -0,0 +1,27 @@
+let gapString = "tryber x aqui!"
+
+function letterSearch (string) {
+  let newString = gapString.split('');
+  let skills = ['HTML', 'CSS', 'JavaScript', 'Shell'];
+  for (let index = 0; index < newString.length; index += 1) {
+    if (newString[index] === "x") {
+      newString[index] = string;
+    }
+  }
+  newString = newString.join('');
+  newString = `${newString}
+  
+  Minhas cinco principais habilidades são:
+  
+    ${skills[0]}
+    ${skills[1]}
+    ${skills[2]}
+    ${skills[3]}
+    ${skills[4]}
+  
+  #goTrybe`
+  
+  console.log(newString);
+}
+
+letterSearch("Luciano");
