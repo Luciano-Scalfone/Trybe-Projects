8008e457225e9ceafee5f6c61467232ba5503940
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-09-03 19:49:15 -0300

    exercise 5 part 1

--- Modulo__10/10_1/exercisespart1.js
+++ Modulo__10/10_1/exercisespart1.js
@@ -6,6 +6,55 @@ function sum(a, b) {
   return a + b;
 }
 
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (item !== arr[i]) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
 
+function myRemoveWithoutCopy(arr, item) {
+  for (let i = 0, len = arr.length; i < len; i += 1) {
+    if (arr[i] === item) {
+      arr.splice(i, 1);
+      i -= 1;
+      len -= 1;
+    }
+  }
+
+  return arr;
+}
+
+function myFizzBuzz(num) {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
+}
+
+const obj1 = {
+  title: 'My Title',
+  description: 'My Description',
+};
+
+const obj2 = {
+  description: 'My Description',
+  title: 'My Title',
+};
+
+const obj3 = {
+  title: 'My Different Title',
+  description: 'My Description',
+};
 
 module.exports = sum;
+module.exports = myRemove;
+module.exports = myRemoveWithoutCopy;
+module.exports = myFizzBuzz;
+module.exports = obj1;
+module.exports = obj2;
+module.exports = obj3;
