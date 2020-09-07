0907d82e72f2cd9686f6acc341ac95c3e590c2a9
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-09-06 22:15:01 -0300

    Exercise 2

--- Modulo__10/10_2/tests/exercise2.test.js
+++ Modulo__10/10_2/tests/exercise2.test.js
@@ -1,11 +1,13 @@
 const { users, findUserById, getUserName } = require('../exercise2.js');
-const { TestScheduler } = require('jest');
 
 describe('Quando a função getUserName é chamada', () => {
-  test('retorna o nome do animal', () => {
+  it('retorna o nome do animal', () => {
     expect.assertions(1);
-    return getUserName(4).then(userName => {
-      expect(userName).toBe('Mark')
-    })
+    return expect(getUserName(4)).resolves.toBe('Mark');
+  })
+
+  it('retorta um erro', () => {
+    expect.assertions(1);
+    return expect(getUserName(8)).rejects.toEqual({ error: `User with 8 not found.` });
   })
 })
