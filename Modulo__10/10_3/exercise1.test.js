cbeea64dc8b0267e1979c4fc4ced801bf56663fb
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-09-07 21:28:06 -0300

    pra fixar 1

--- Modulo__10/10_3/exercise1.test.js
+++ Modulo__10/10_3/exercise1.test.js
@@ -3,9 +3,23 @@ function randomNumber() {
   return r;
 }
 
-const divisible = (divisor) => randomNumber() % divisor === 0;
+const divisible = (divisor) => (randomNumber() % divisor) === 0;
 
 test('number is divisible', () => {
-  divisible()
-  expect(randomNumber).toHaveBeenCalled()
+  randomNumber = jest.fn()
+  .mockReturnValue(2)
+  .mockReturnValueOnce(4)
+  .mockReturnValueOnce(3);
+
+  divisible();
+  expect(randomNumber).toHaveBeenCalledTimes(1);
+
+  expect(divisible(2)).toBe(true);
+  expect(randomNumber).toHaveBeenCalledTimes(2);
+
+  expect(divisible(2)).toBe(false);
+  expect(randomNumber).toHaveBeenCalledTimes(3);
+
+  expect(divisible(2)).toBe(true);
+  expect(randomNumber).toHaveBeenCalledTimes(4);
 })
