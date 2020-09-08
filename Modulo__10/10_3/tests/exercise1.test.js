7c26440f8657e504a5e89b0e62042819c3ea3266
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-09-08 18:51:37 -0300

    exercise2

--- Modulo__10/10_3/tests/exercise1.test.js
+++ Modulo__10/10_3/tests/exercise1.test.js
@@ -1,10 +1,18 @@
 const number = require('../exercises');
 
 describe('#isDivisible', () => {
-  number.randomNumber = jest.fn().mockReturnValue(10);
+  number.randomNumber = jest.fn()
+  .mockReturnValue(10)
+  .mockImplementationOnce((a, b) => a / b);
+
+  it('return division', () => {
+    expect(number.randomNumber(2, 2)).toBe(1);
+    expect(number.randomNumber).toHaveBeenCalledWith(2, 2);
+    expect(number.randomNumber).toHaveBeenCalledTimes(1);
+  })
 
   it('return number 10', () => {
     expect(number.randomNumber()).toBe(10);
-    expect(number.randomNumber).toHaveBeenCalledTimes(1);
+    expect(number.randomNumber).toHaveBeenCalledTimes(2);
   })
 })
