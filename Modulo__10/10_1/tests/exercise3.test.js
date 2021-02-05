0c69bc0d65efd646ff7ed8080e2f3460620f310a
Luciano Scalfone <luciano.scalfone@gmail.com>
2021-02-05 09:47:43 -0300

    bloco 26

--- Modulo__10/10_1/tests/exercise3.test.js
+++
@@ -1,7 +0,0 @@
-const myRemoveWithoutCopy = require('../exercisespart1.js');
-
-test('Function myRemoveWithoutCopy', () => {
-  expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
-  expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
-  expect(myRemoveWithoutCopy([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
-})
